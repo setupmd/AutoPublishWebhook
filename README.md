4d18c9c63abaac09bfa2158510b5640df886d50f
mufinlive <mufin@mufin.live>
2022-07-27 16:59:59 +0200

    Initial setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# AutoPublishWebhook
+An insanely simple Discord bot to automatically publish webhook messages sent in news channels.
+
+### Compiling
+
+```bash
+$ mvn install
+$ mvn package
+```
+
+File will be in `target/AutoPublishWebhook.jar`.
--- src/main/java/md/setup/autopublishwebhook/AutoPublishWebhook.java
+++ src/main/java/md/setup/autopublishwebhook/AutoPublishWebhook.java
@@ -1,2 +1,33 @@
-package md.setup.autopublishwebhook;public class AutoPublishWebhook {
+package md.setup.autopublishwebhook;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.NewsChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.security.auth.login.LoginException;
+
+public class AutoPublishWebhook extends ListenerAdapter {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(AutoPublishWebhook.class);
+
+  public static void main(String[] args) throws LoginException {
+    JDABuilder
+       .createDefault(System.getenv("discord.token"))
+       .setEnabledIntents(GatewayIntent.MESSAGE_CONTENT)
+       .addEventListeners(new AutoPublishWebhook())
+       .build();
+  }
+
+  @Override
+  public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+    if(!(event.getChannel() instanceof NewsChannel channel)) return;
+    if(event.getMessage().isWebhookMessage()) channel.crosspostMessageById(event.getMessageId()).queue();
+    LOGGER.info("Published message {} in channel {}.", event.getMessageId(), channel.getId());
+  }
 }
